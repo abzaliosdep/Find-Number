bd481406c105ab8ba5009aa3db2dbe35fd5afc96
Abzal <bukharbay.abzal@gmail.com>
2022-10-04 20:02:44 +0600

    3.0

--- Find Number/GameViewController.swift
+++ Find Number/GameViewController.swift
@@ -17,7 +17,14 @@ class GameViewController: UIViewController {
     
     @IBOutlet weak var timerLabel: UILabel!
     
-    lazy var game = Game(countItems: buttons.count)
+    @IBOutlet weak var newGameButton: UIButton!
+    lazy var game = Game(countItems: buttons.count,time: 30) { [weak self] (status, time) in
+        
+        guard let self = self else {return}
+        
+        self.timerLabel.text = time.secondsToString()
+        self.updateInfoGame(with: status)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -34,11 +41,18 @@ class GameViewController: UIViewController {
         updateUI()
     }
     
+    @IBAction func newGame(_ sender: UIButton) {
+        game.newGame()
+        sender.isHidden = true
+        setupScreen()
+    }
+    
     private func setupScreen() {
         
         for index in game.items.indices {
             buttons[index].setTitle(game.items[index].title, for: .normal)
-            buttons[index].isHidden = false
+            buttons[index].alpha = 1
+            buttons[index].isEnabled = true
         }
         
         nextDigit.text = game.nextItem?.title
@@ -46,15 +60,38 @@ class GameViewController: UIViewController {
     
     private func updateUI() {
         for index in game.items.indices {
-            buttons[index].isHidden = game.items[index].isFound
+            buttons[index].alpha = game.items[index].isFound ? 0 : 1
+            buttons[index].isEnabled = !game.items[index].isFound
+            
+            if game.items[index].isError {
+                UIView.animate(withDuration: 0.3) { [weak self] in
+                    self?.buttons[index].backgroundColor = .red
+                } completion: { [weak self] (_) in
+                    self?.buttons[index].backgroundColor = .white
+                    self?.game.items[index].isError = false
+                }
+            }
         }
         
         nextDigit.text = game.nextItem?.title
         
-        if game.status == .win {
-            statusLabel.text = "You win!"
-            statusLabel.textColor = .green
-        }
+        updateInfoGame(with: game.status)
     }
     
+    private func updateInfoGame(with status:StatusGame) {
+        switch status {
+        case .start:
+            statusLabel.text = "Game started"
+            statusLabel.textColor = .black
+            newGameButton.isHidden = true
+        case .win:
+            statusLabel.text = "You win!"
+            statusLabel.textColor = .green
+            newGameButton.isHidden = false
+        case .lose:
+            statusLabel.text = "You lose!"
+            statusLabel.textColor = .red
+            newGameButton.isHidden = false
+        }
+    }
 }
